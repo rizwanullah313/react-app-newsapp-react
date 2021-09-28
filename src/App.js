6a7b6f6d99a3369e46f63dea3d80a186c7fab9c1
rizwanullah313 <ru323091@gmail.com>
2021-09-28 23:04:01 +0500

    create different Clasess and update Read me File

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
+   // let a = "rizwan ullah";
     return (
       <div>
-        hello first class based component
+        {/* hello first class based component {a} */}
+        <Navbar />
+        <News />
       </div>
     )
   }
