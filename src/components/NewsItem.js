7cac25975a565f4f369b8e31ff9f62ce502ffa62
rizwanullah313 <ru323091@gmail.com>
2021-09-29 01:00:50 +0500

    Copy and Print some news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,17 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+  
     render() {
-        let {title, description} = this.props;
+        let {title, description, imgUrl, newUrl} = this.props;
         return (
-            <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src="https://image.cnbcfm.com/api/v1/image/106948622-1632839136144astro-press.PNG?v=1632839257" className="card-img-top" alt="..." />
+            <div className="container my-3">
+                <div className="card my-3" style={{width: "18rem"}}>
+                    <img src={imgUrl} className="card-img-top" alt="..." />
                     <div className ="card-body">
                     <h5 className ="card-title">{title}</h5>
                     <p className ="card-text">{description}</p>
-                    <a href="NewDetails" className ="btn btn-primary">Go somewhere</a>
+                    <a href={newUrl} target="_blank" rel="noreferrer"  className ="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>            
                 </div>
